07cea416554ed684e956eabe7c8b44438c71e68e
Mário Jesus <mrfjesus@outlook.pt>
2023-05-11 19:42:38 +0100

    Primeiro Push

--- Jenkins/Lib/Class1.cs
+++ Jenkins/Lib/Class1.cs
@@ -1,7 +1,29 @@
-﻿namespace Lib
+﻿using System.ComponentModel.Design;
+
+namespace Lib
 {
-    public class Class1
+    public class Saudacao
     {
+        public static string Cumprimentar(String value, DateTime dateTime)
+        {
+            String result = "";
+            DateTime dt = DateTime.Now;
+            if(dt.Hour<=12)
+            {
+                result = "Bom dia" + value;
+            }
+            else if(dt.Hour<=20)
+            {
+                    result = "Boa tarde" + value;
+
+            }
+                else                
+                {
+                    result = "Boa noite" + value;
+
+                }
+            return result;
+        }
 
     }
 }
--- Jenkins/xUnit/UnitTest1.cs
+++ Jenkins/xUnit/UnitTest1.cs
@@ -1,10 +1,31 @@
+using Xunit;
+
 namespace xUnit
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Test_BomDia()
         {
+            DateTime dt = new DateTime(2023, 5, 11, 0, 0, 0);
+            string resultadoesperado = "Bom dia Mario Jesus";
+            Assert.Equal(resultadoesperado, Lib.Saudacao.Cumprimentar("Mario Jeus", dt));
+
+        }
+        [Fact]
+        public void Test_BoaTarde()
+        {
+            DateTime dt = new DateTime(2023, 5, 11, 0, 0, 0);
+            string resultadoesperado = "Boa tarde Mario Jesus";
+            Assert.Equal(resultadoesperado, Lib.Saudacao.Cumprimentar("Mario Jeus", dt));
+
+        }
+        [Fact]
+        public void Test_BoaNoite()
+        {
+            DateTime dt = new DateTime(2023, 5, 11, 0, 0, 0);
+            string resultadoesperado = "Boa noite Mario Jesus";
+            Assert.Equal(resultadoesperado, Lib.Saudacao.Cumprimentar("Mario Jeus", dt));
 
         }
     }
